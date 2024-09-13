e6c001104a8b6e65dd62f338b5d862142145b1d0
qialex <qialex.i@gmail.com>
2024-09-14 01:46:23 +0200

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # task-ts-less
+
+## Prerequisites
+
+- Node.js
+- npm
+
+## Scripts
+
+### Run dev
+
+``` npm start ```
+
+### Build
+
+Build into the ```dist``` output directory.
+
+``` npm run build ```
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = app;
+function app() {
+    console.log("TypeScript works!");
+}
+//# sourceMappingURL=index.js.map
